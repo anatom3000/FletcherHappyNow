bc8fcdf765a013b124d38c9cb6a50463096824be
anatom <49984698+anatom3000@users.noreply.github.com>
2020-07-11 16:52:46 +0200

    Added dependancies (LibGUI) and renamed class names.

--- src/main/java/fr/anatom3000/fletcherhappynow/FletcherHappyNowMod.java
+++ src/main/java/fr/anatom3000/fletcherhappynow/FletcherHappyNowMod.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package fr.anatom3000.fletcherhappynow;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class FletcherHappyNowMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
